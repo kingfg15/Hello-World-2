65ff307ead0555e92c497b78cfd9e70115f50c57
Francis King <kingfg15@mounties.mansfield.edu>
2022-03-03 20:39:00 +0000

    This is how I want it to stay.

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ for(let i = 0; i < 100; i++) {
   test.innerHTML = '-> AAAAAAAAHHHHHHH! A DRAGON! <-';
   document.body.appendChild(test);
 }
+
+
+const test = document.createElement('p');
+test.innerHTML = 'AAAAAAAAHHHHHHH! A DRAGON!';
+document.body.appendChild(test);
+*/
